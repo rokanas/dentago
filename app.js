64ee2f0753bfa7a029d5d200c1f9088b2cdd2a3b
Ionel Pop <ionelalexandru.pop@gmail.com>
2023-11-17 01:12:08 +0100

    #1 Add app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+/**
+ * The dentist API allows dentist to:
+ * - Register open slots
+ * - Be notified when slots have been booked
+ * - Be notified when bookings have been cancelled
+ */
+
+// TODO: Decide how will the system send notifications
+
+console.log('Oh, hi there! 😎');
