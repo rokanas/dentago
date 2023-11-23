b504fa1a3a08f9abd914f1897b9dfa9d6095429c
Teo Portase <teoportase@gmail.com>
2023-11-23 12:35:19 +0100

    #2 Add a basic Google Maps map
    Also related to issue #1

--- patient-ui/src/views/HomeView.vue
+++ patient-ui/src/views/HomeView.vue
@@ -6,7 +6,10 @@
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
       <br>
     </div>
-
+    <div>
+      Google Maps <br>
+      <iframe width="600" height="450" style="border:0" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/view?zoom=10&center=57.7089%2C11.9746&key=AIzaSyDJI6Bl-wI_G2ITSbo18kUAytoJPSp_q3g"></iframe>
+    </div>
     <div class="banner"> Contact info and stuff </div>
   </div>
 </template>
