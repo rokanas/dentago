f4df82f1986dadedeea9eaf1c7c5240a79ac48f9
Ionel Pop <ionelalexandru.pop@gmail.com>
2023-11-28 19:16:49 +0100

    #5 Update clinic.js
    Remove superfluous comment in the clinic schema

--- models/clinic.js
+++ models/clinic.js
@@ -2,12 +2,6 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-/**
- * Location makes use of GeoJSON,
- * a format for storing geographic points and polygons
- * https://mongoosejs.com/docs/geojson.html.
- * This helps achieve efficient spatial queries.
- */
 const clinicSchema = new Schema({
     clinicId: {
         type: String,
