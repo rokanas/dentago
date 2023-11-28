cfecc3ff6921f70e7fadf7b14f8454ee59d45f14
Erik <elindstr@student.chalmers.se>
2023-11-28 08:47:14 +0100

    Add '.gitignore' file
    This commit adds the .gitignore file to prevent unwanted files from being pushed to remote.

---
+++ src/main/java/org/example/MqttSubscriber.java
@@ -0,0 +1,8 @@
+package org.example;
+import org.eclipse.paho.client.mqttv3.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world! This is a Gradle project! :)");
+    }
+}
