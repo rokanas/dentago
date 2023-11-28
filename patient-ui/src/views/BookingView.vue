5b926c9535e4130ce458affdb54ace9374482848
Teo Portase <teoportase@gmail.com>
2023-11-28 10:40:48 +0100

    #6 Create redirect from clinics on map to booking page
    As well as creating a very simple page for the booking.

---
+++ patient-ui/src/views/BookingView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <HeaderBar></HeaderBar>
+
+        <h1> Clinic id = {{ clinicId }}</h1>
+    </div>
+</template>
+
+<script>
+import HeaderBar from '@/components/HeaderBar.vue'
+
+export default {
+    components: {
+        HeaderBar
+    },
+    props: {
+        clinicId: String
+    }
+}
+</script>
