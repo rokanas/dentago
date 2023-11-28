d02a779a7a6220ec852ba4b3a3a284d2d2c56346
Teo Portase <teoportase@gmail.com>
2023-11-28 11:19:07 +0100

    #6 Add prototype time slot table

--- patient-ui/src/views/BookingView.vue
+++ patient-ui/src/views/BookingView.vue
@@ -3,6 +3,51 @@
         <HeaderBar></HeaderBar>
 
         <h1> Clinic id = {{ clinicId }}</h1>
+
+        <div>
+            <table>
+                <tr>
+                    <th> Times </th>
+                    <th> Sunday </th>
+                    <th> Monday </th>
+                    <th> Tuesday </th>
+                    <th> Wednesday </th>
+                    <th> Thursday </th>
+                    <th> Friday </th>
+                    <th> Saturday </th>
+                </tr>
+                <tr>
+                    <td> 8:00 </td>
+                </tr>
+                <tr>
+                    <td> 9:00 </td>
+                </tr>
+                <tr>
+                    <td> 10:00 </td>
+                </tr>
+                <tr>
+                    <td> 11:00 </td>
+                </tr>
+                <tr>
+                    <td> 12:00 </td>
+                </tr>
+                <tr>
+                    <td> 13:00 </td>
+                </tr>
+                <tr>
+                    <td> 14:00 </td>
+                </tr>
+                <tr>
+                    <td> 15:00 </td>
+                </tr>
+                <tr>
+                    <td> 16:00 </td>
+                </tr>
+                <tr>
+                    <td> 17:00 </td>
+                </tr>
+            </table>
+        </div>
     </div>
 </template>
 
@@ -10,11 +55,21 @@
 import HeaderBar from '@/components/HeaderBar.vue'
 
 export default {
+    data() {
+        return {
+            days: 7
+        }
+    },
     components: {
         HeaderBar
     },
     props: {
         clinicId: String
+    },
+    methods: {
+        getClinic() {
+            // will get information about the clinic
+        }
     }
 }
 </script>
