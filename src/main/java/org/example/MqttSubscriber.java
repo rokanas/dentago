99e623b8ff628afe38e51a5bff5ffb8fc0eb9e4e
Erik <elindstr@student.chalmers.se>
2023-11-28 09:00:12 +0100

    Delete Existing Software Artifacts
    This commit deletes any software artifacts related to the previous Java project setup using Gradle.

--- src/main/java/org/example/MqttSubscriber.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-import org.eclipse.paho.client.mqttv3.*;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world! This is a Gradle project! :)");
-    }
-}
