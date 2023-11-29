1c8e6b99d504336e998f7304143857d1dad9d9be
Konstantinos Rokanas <rokanas@student.chalmers.se>
2023-11-29 19:03:55 +0100

    #3 Add CI environment variables for MQTT broker
    This commit alters mqtt.js so that if no .env file is found, it can acquire its environment variables from the gitlab CI environment variables (for testing in the pipeline).

--- mqtt.js
+++ mqtt.js
@@ -2,7 +2,7 @@ const mqtt = require('mqtt');
 
 /*====================  MQTT SETUP  ==================== */
 
-const broker = 'mqtt://127.0.0.1:1883'; // TODO: change to hosted mosquitto address
+const broker = process.env.MOSQUITTO_URI || process.env.CI_MOSQUITTO_URI;
 
 // connect to the MQTT broker
 const client = mqtt.connect(broker);
