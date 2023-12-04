1fdfb53cf882e9daebe0363ec494815faf677f93
Konstantinos Rokanas <rokanas@student.chalmers.se>
2023-12-05 00:13:58 +0100

    #2 Add initial patient schema
    This commit adds an initial mongoose schema file for patient resource.

---
+++ patient.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    id: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    fullName: {
+        type: String,
+        // get fullName by concatenating first and last names
+        get: function () {
+            return `${this.firstName} ${this.lastName}`;
+        },
+        // set new fullName by splitting first and last names
+        set: function (fullName) {
+            const names = fullName.split(' ');
+            this.firstName = names[0] || '';
+            this.lastName = names[1] || '';
+        }
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    refreshToken: {
+        type: String,
+        unique: true,
+        required: true
+    }
+});
+
+module.exports = mongoose.model("User", userSchema);
