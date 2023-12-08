0a9cef79252c47a7677a475f844ddf77a6b0402a
Konstantinos Rokanas <rokanas@student.chalmers.se>
2023-12-09 00:44:55 +0100

    #1 Remove unnecessary imports and functions
    This commit removes imports and functions relating to Express, as the service will not involve REST API.

--- logServer.js
+++ logServer.js
@@ -1,17 +1,14 @@
-const express = require('express');         // web app framework
 const mongoose = require('mongoose');       // mongoDB data structuring and schema validation
 const path = require('path');               // handles file paths
-const mqtt = require('./mqtt.js');          // contains mqtt functions
+//const mqtt = require('./mqtt.js');          // contains mqtt functions
 
 // import environmental variables
 require('dotenv').config();                 // load environmental variables from .env file to process.env object
 
 const mongoURI = process.env.MONGODB_URI || process.env.CI_MONGODB_URI;
-const host = process.env.HOST || process.env.CI_HOST;
-const port = process.env.PORT || process.env.CI_PORT;
 
 // subscribe to mqtt topics
-mqtt.subscribe("dentago/");
+// mqtt.subscribe("dentago/");
 
 // Connect to MongoDB
 mongoose.connect(mongoURI)
@@ -23,15 +20,3 @@ mongoose.connect(mongoURI)
     console.error(err.stack);
     process.exit(1);
   });
-
-// create Express app
-const app = express();
-
-app.listen(port, function(err) {
-    if (err) throw err;
-    console.log(`Express server listening on port ${port}, in ${env} mode`);
-    console.log(`Backend: ${host}:${port}/api/`);
-    console.log(`Frontend (production): ${host}:${port}/`);
-});
-
-module.exports = app;
