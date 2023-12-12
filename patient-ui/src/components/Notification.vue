b2d0623f72d6418cf9d08c349d98cc180eccd9c5
Teo Portase <teoportase@gmail.com>
2023-12-12 16:56:49 +0100

    #3 Create notification component + fix header bar

---
+++ patient-ui/src/components/Notification.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="notif">
+        <div id="title"> {{ title }} </div>
+        <div id="description"> {{ description }} </div>
+        <br>
+    </div>
+</template>
+
+<style>
+#notif {
+    border: 12px;
+}
+
+#title {
+    font-weight: bold;
+    font-size: large;
+}
+</style>
+
+<script>
+export default {
+    props: {
+        title: String,
+        description: String
+    }
+}
+</script>
