0ce8e8f6866d2b443b39e057a32d20ea979d4411
Teo Portase <teoportase@gmail.com>
2023-12-13 15:13:07 +0100

    #1 Implement tests for more endpoints

--- locustfile.py
+++ locustfile.py
@@ -1,7 +1,40 @@
 from locust import HttpUser, task
 
-class Dentago(HttpUser):
+# TODO: https://docs.locust.io/en/stable/running-distributed.html - make processes distributed to help with CPU/memory usage
+
+class Dentago(FastHttpUser):
     @task
     def dentago(self):
+# ------------------------------ CLINICS -------------------------------------------------
+        # GET /clinics --- on Home page (Google Map)
         self.client.get("/clinics")
-        self.client.get("/clinics/greenHillZoneClinic")
+
+        # GET /clinics/:clinicId --- on Booking page for a specific clinic
+        self.client.get("/clinics/657304e661d1c9f248318308")
+
+# ------------------------------ TIMESLOTS -----------------------------------------------
+
+        # GET /clinics/:clinicId/timeslots --- on Booking page to showcase the (un)available timeslots
+        self.client.get("/clinics/657304e661d1c9f248318308/timeslots")
+
+        # PATCH /clinics/:clinicId/timeslots/:timeslotId --- on Booking page when booking an appointment
+        self.client.patch(
+            "/clinics/657304e661d1c9f248318306/timeslots/657304e861d1c9f248318323",
+            {"instruction": "BOOK", "patient": "657378334875f85b1a0233c2"}
+            )
+
+        # PATCH /clinics/:clinicId/timeslots/:timeslotId --- on Profile page when cancelling an appointment
+        self.client.patch(
+            "/clinics/657304e661d1c9f248318306/timeslots/657304e861d1c9f248318323",
+            {"instruction": "CANCEL", "patient": "657378334875f85b1a0233c2"}
+            )
+
+# ------------------------------ PATIENTS ------------------------------------------------
+
+        # TODO:
+        # GET /patients/ --- on Profile page get specific patient
+
+
+# ---------------------------- NOTIFICATIONS ---------------------------------------------
+
+        # TODO
