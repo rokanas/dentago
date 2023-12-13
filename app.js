ae6fdeffab3bc02fec28a33a062381749b082bde
Erik <elindstr@student.chalmers.se>
2023-12-13 01:23:57 +0100

    #2 Add Basic MQTT File Structure
    This commit adds the basic MQTT implementatin as well as some additional file setup to facilitate further additions to the CLI tool.

---
+++ app.js
@@ -0,0 +1,81 @@
+// Read input
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+// Variables
+// const clinicId = '';                 // The corresponding Clinic ID --> //TODO: fetch after successful login
+const clinicId = 'greenHillZoneClinic'; // Placeholder hardcoded value - remove
+const digitRegex = /^\d+$/;             // Checks that a String only contains digits
+
+// MQTT stuffs
+const mqtt = require('mqtt');
+const broker = 'mqtt://test.mosquitto.org/:1883';
+const hiveBroker = 'mqtt://broker.hivemq.com/:1883';
+const mqttClient = mqtt.connect(broker);
+
+// TEST TOPICS
+const mockApiPublish = 'dentago/dentist/'; 
+const mockApiSubscribe = mockApiPublish + clinicId;
+
+// MQTT topics
+MQTT_TOPICS = {
+    getTimeslots: 'dentago/availability/',
+    createClinic: 'dentago/dentist/creation/clinics',
+    createDentist: 'dentago/dentist/creation/dentists',
+    createTimeslot: 'dentago/dentist/creation/timeslot',
+    assignTimeslot: 'dentago/dentist/assignment/timeslot',
+    bookingNotifications: 'dentago/booking/'
+}
+
+const authLogin = 'dentago/dentist/login/'; // TODO: implement this for the login functionality
+
+
+//========================== MQTT EVENT LISTENERS ==========================//
+mqttClient.on('connect', () => {
+    console.log('Connected to MQTT broker');
+
+    // Append the clinicId to all topics and subscribe to each
+    mqttClient.subscribe(Object.values(MQTT_TOPICS).map(topic => topic + clinicId), (error, granted) => {
+        if(!error) {
+            granted.forEach(key => {
+                console.log(`Subscribed to messages on: ${key.topic}`);
+            });
+        }
+    });
+});
+
+mqttClient.on('message', (topic, message) => {
+    try {
+        const payload = JSON.parse(message);
+        console.log('\n' + topic);
+        console.log(payload);
+
+    } catch (error) {
+        console.log("Error when processing MQTT message: ", error);
+    }
+});
+
+mqttClient.on('error', (error) => {
+    console.error('MQTT connection error: ', error);
+});
+
+mqttClient.on('close', () => {
+    // "\n" due to Windows cmd prompt
+    console.log('\nClient disconnected from MQTT broker');
+});
+
+mqttClient.on('reconnect', () => {
+    console.log('Reconnected to MQTT broker');
+}); 
+
+process.on('SIGINT', () => {
+    console.log('Closing MQTT connection...');
+    // End MQTT connection and exit process using success codes for both
+    mqttClient.end({ reasonCode: 0x00 }, () => {
+        console.log('MQTT connection closed');
+        process.exit(0);
+    });
+});
