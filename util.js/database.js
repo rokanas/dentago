942faaf31703956711463303d5b009a87d1ff09e
David Rocha <david.miguel.rocha@hotmail.com>
2023-12-13 02:26:41 +0100

    #10 Expand circuit breaker for cancelTimeslot, refactor
    Extended the circuit breaker to include cancelTimeslot, Refactores the circuit breaker so that the utility functions are in their own file

--- util.js/database.js
+++ util.js/database.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 const mongoURI = process.env.MONGODB_URI;
 
 export async function connectDatabase() {
+  console.log("Attempting to connect to database...");
   await mongoose
     .connect(mongoURI, {})
     .catch((err) => {
@@ -12,4 +13,15 @@ export async function connectDatabase() {
       console.error(err.stack);
     })
     .then(console.log("Connected to MongoDB!"));
+}
+
+export async function disconnectDatabase() {
+  await mongoose
+    .disconnect()
+    .catch((err) => {
+      console.error(err);
+      console.error(`Failed to disconnect to MongoDB with URI: ${mongoURI}`);
+      console.error(err.stack);
+    })
+    .then(console.log("Database service was disconnected."));
 }
