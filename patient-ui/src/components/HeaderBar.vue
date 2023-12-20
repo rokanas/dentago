ad23750b3a4799722c0a9a64e2d0ff61e3a687ba
Ionel Pop <ionelalexandru.pop@gmail.com>
2023-12-20 02:11:06 +0100

    #12 Create footer bar and update header bar
    Header bar is now displayed in the general layout

--- patient-ui/src/components/HeaderBar.vue
+++ patient-ui/src/components/HeaderBar.vue
@@ -1,36 +1,54 @@
 <template>
     <div id="navbar">
-        <router-link id="title" to="/"> Header Bar </router-link>
-        <button> Notifications </button>
-        <button @click="redirect()"> Profile </button>
+        <button class="btn btn-link" @click="navigateTo('/')" data-toggle="tooltip" data-placement="bottom" title="Home">
+            <span class="material-symbols-outlined">
+                home
+            </span>
+        </button>
+        <button class="btn btn-link" data-toggle="tooltip" data-placement="bottom" title="Notifications">
+            <span v-if="notifications === null" class="material-symbols-outlined">
+                notifications
+            </span>
+            <span v-else class="material-symbols-outlined">
+                notifications_unread
+            </span>
+        </button>
+        <button class="btn btn-link" @click="redirect()" data-toggle="tooltip" data-placement="bottom" title="Profile">
+            <span class="material-symbols-outlined">
+                account_circle
+            </span>
+        </button>
     </div>
 </template>
-
-<style scoped>
-#navbar{
-    display: flex;
-    background-color: #FFA686;
-    filter: drop-shadow(0px 2px 2px #000000);
-    justify-content: space-evenly;
-    padding: 1%;
-}
-
-#title{
-    font-size: larger;
-}
-</style>
 
 <script>
 export default {
     data() {
         return {
-            username: 'ratKing' // placeholder value
+            username: 'ratKing', // placeholder value
+            notifications: null,
         }
     },
     methods: {
-        redirect(){
+        navigateTo(route) {
+            this.$router.push(route);
+        },
+        redirect() {
             this.$router.push('/user/' + this.username);
         }
     }
 }
 </script>
+
+<style scoped>
+.material-symbols-outlined {
+  color: black;
+  font-size: 1.8em; /* Adjust the size as needed */
+  /* transition: transform 0.2s, color 0.2s; */
+}
+
+.material-symbols-outlined:hover {
+  /* transform: scale(1.2); Adjust the scale factor as needed for the pop effect */
+  color: #333; /* Darker shade of black for hover effect */
+}
+</style>
