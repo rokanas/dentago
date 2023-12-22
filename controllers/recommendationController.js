0f195f1484cbe33119fead921c8e44a725d2ba46
Konstantinos Rokanas <rokanas@student.chalmers.se>
2023-12-23 00:16:31 +0200

    #8 Complete implementation of generateRecommendations function
    This commit completes implementation so that now a list of recommended timeslots is now correctly generated based on available timeslots and patient preferences.

--- controllers/recommendationController.js
+++ controllers/recommendationController.js
@@ -1,17 +1,49 @@
 const express = require('express');
 const Patient = require('../models/patient');
+const Timeslot = require('../models/timeslot');
 const router = express.Router();
 
-async function parsePreferences(preferences) {
-    // extract non-empty days from preferences object
-    const days = Object.keys(preferences).filter(
-        (day) =>
-        Array.isArray(preferences[day]) &&
-        preferences[day].length > 0
-    );
-  
-    return days;
-};
+// function that returns timeslots filtered according to patient preferences
+async function generateRecommendations(preferences, timeslots) {
+    try {
+        // extract non-empty days from preferences object
+        const days = Object.keys(preferences).filter(
+            (day) =>
+            Array.isArray(preferences[day]) &&  // check that attribute is an array of times
+            preferences[day].length > 0         // check that day contains > 0 preferred hours
+        );
+
+        // filter timeslots aaccording to patient preferences
+        const recommendedTimeslots = timeslots.filter((timeslot) => {
+
+            // parse name of day from timeslot's startTime
+            const day = timeslot.startTime.toLocaleDateString('en-US', { weekday: 'long' });
+            
+            // check if the timeslot's day is present in patient preferences
+            if (days.includes(day)) {
+                // if so, extract the patient's preferred times
+                const recommendedTimes = preferences[day];
+        
+                // extract hour from timeslot's startTime
+                const startHour = timeslot.startTime.getUTCHours();
+                
+                // include/exclude timeslot depending on whether start time corresponds to patient's preferred time
+                return recommendedTimes.includes(startHour);
+                
+            } else {
+                // if the timeslot's day is not present in patient preferences, exclude it
+                return false;
+            }
+        });
+
+        // return array of timeslot objects
+        return recommendedTimeslots;
+    
+    } catch(err) {
+        // log error
+        console.log(err.message);
+    }
+}
 
 /*====================  ROUTE HANDLERS  ==================== */
 /*===================  RECOMMENDATIONS ===================== */
@@ -61,9 +93,16 @@ router.get('/patients/:patient_id/recommendations', async (req, res) => {
 
         // extract preferences object from patient resource
         const preferences = patient.schedulePreferences.toObject();
+
+        // fetch timeslots (placeholder)
+        // TODO: relocate function to availability service
+        const timeslots = await Timeslot.find();
         
-        const days = await parsePreferences(preferences);
-        res.status(200).json({ days: days });
+        // call function to filter timeslots according to patient preferences
+        const recommendations = await generateRecommendations(preferences, timeslots);
+        
+        // return array of recommendations objects
+        res.status(200).json({ recommendations: recommendations });
 
     } catch(err) {
         res.status(500).json({ Error: err.message }); // internal server error
