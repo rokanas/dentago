1bc3a6b37105b8f5fe514c4f7e9dab40f69d6f51
Konstantinos Rokanas <rokanas@student.chalmers.se>
2023-12-23 21:15:52 +0200

    #8 Merge recommendation and patient schema model files

--- models/patient.js
+++ models/patient.js
@@ -1,6 +1,85 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const preference = require('./preference');
+
+const preferenceSchema = new Schema({
+    Monday: [{
+        type: Number,
+        min: 0,
+        max: 23,
+        unique: false,
+        required: false,
+        validate : {
+            validator : Number.isInteger,
+            message   : '{VALUE} is not an integer value'
+          }
+    }],
+    Tuesday: [{
+        type: Number,
+        min: 0,
+        max: 23,
+        unique: false,
+        required: false,
+        validate : {
+            validator : Number.isInteger,
+            message   : '{VALUE} is not an integer value'
+          }
+    }],
+    Wednesday: [{
+        type: Number,
+        min: 0,
+        max: 23,
+        unique: false,
+        required: false,
+        validate : {
+            validator : Number.isInteger,
+            message   : '{VALUE} is not an integer value'
+          }
+    }],
+    Thursday: [{
+        type: Number,
+        min: 0,
+        max: 23,
+        unique: false,
+        required: false,
+        validate : {
+            validator : Number.isInteger,
+            message   : '{VALUE} is not an integer value'
+          }
+    }],
+    Friday: [{
+        type: Number,
+        min: 0,
+        max: 23,
+        unique: false,
+        required: false,
+        validate : {
+            validator : Number.isInteger,
+            message   : '{VALUE} is not an integer value'
+          }
+    }],
+    Saturday: [{
+        type: Number,
+        min: 0,
+        max: 23,
+        unique: false,
+        required: false,
+        validate : {
+            validator : Number.isInteger,
+            message   : '{VALUE} is not an integer value'
+          }
+    }],
+    Sunday: [{
+        type: Number,
+        min: 0,
+        max: 23,
+        unique: false,
+        required: false,
+        validate : {
+            validator : Number.isInteger,
+            message   : '{VALUE} is not an integer value'
+          }
+    }]
+});
 
 const patientSchema = new Schema({
     id: {
@@ -44,7 +123,7 @@ const patientSchema = new Schema({
         required: false
     }],
     schedulePreferences: {
-        type: preference.schema,
+        type: preferenceSchema,
         default: null,
         required: false
     }
