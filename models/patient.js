30fa91b48e863aaf56acb0a49bf8233409eb7d06
Ionel Pop <ionelalexandru.pop@gmail.com>
2024-01-08 15:26:13 +0100

    #20 Update schemas

--- models/patient.js
+++ models/patient.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const notificationSchema = require('./notification.js');
 
 const patientSchema = new Schema({
     id: {
@@ -37,13 +38,96 @@ const patientSchema = new Schema({
         unique: true
     },
     notifications: [{
-        type: Schema.Types.ObjectId, ref: "Notification",
+        type: notificationSchema.schema, 
+        ref: "Notification",
+        default: [],
         required: false
     }],
     schedulePreferences: {
-        type: String,
+        type: preferenceSchema,
+        default: null,
         required: false
     }
+});
+
+const preferenceSchema = new Schema({
+    Monday: [{
+        type: Number,
+        min: 0,
+        max: 23,
+        unique: false,
+        required: false,
+        validate : {
+            validator : Number.isInteger,
+            message   : '{VALUE} is not an integer value'
+          }
+    }],
+    Tuesday: [{
+        type: Number,
+        min: 0,
+        max: 23,
+        unique: false,
+        required: false,
+        validate : {
+            validator : Number.isInteger,
+            message   : '{VALUE} is not an integer value'
+          }
+    }],
+    Wednesday: [{
+        type: Number,
+        min: 0,
+        max: 23,
+        unique: false,
+        required: false,
+        validate : {
+            validator : Number.isInteger,
+            message   : '{VALUE} is not an integer value'
+          }
+    }],
+    Thursday: [{
+        type: Number,
+        min: 0,
+        max: 23,
+        unique: false,
+        required: false,
+        validate : {
+            validator : Number.isInteger,
+            message   : '{VALUE} is not an integer value'
+          }
+    }],
+    Friday: [{
+        type: Number,
+        min: 0,
+        max: 23,
+        unique: false,
+        required: false,
+        validate : {
+            validator : Number.isInteger,
+            message   : '{VALUE} is not an integer value'
+          }
+    }],
+    Saturday: [{
+        type: Number,
+        min: 0,
+        max: 23,
+        unique: false,
+        required: false,
+        validate : {
+            validator : Number.isInteger,
+            message   : '{VALUE} is not an integer value'
+          }
+    }],
+    Sunday: [{
+        type: Number,
+        min: 0,
+        max: 23,
+        unique: false,
+        required: false,
+        validate : {
+            validator : Number.isInteger,
+            message   : '{VALUE} is not an integer value'
+          }
+    }]
 });
 
 module.exports = mongoose.model("Patient", patientSchema);
