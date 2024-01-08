dc9212fbda32aa77c1ac96fb71174a84a6b8d38b
Ionel Pop <ionelalexandru.pop@gmail.com>
2024-01-08 15:32:17 +0100

    #13 Update schemas

--- models/patient.js
+++ models/patient.js
@@ -2,6 +2,54 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const notificationSchema = require('./notification.js');
 
+const patientSchema = new Schema({
+    id: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    fullName: {
+        type: String,
+        // get fullName by concatenating first and last names
+        get: function () {
+            return `${this.firstName} ${this.lastName}`;
+        },
+        // set new fullName by splitting first and last names
+        set: function (fullName) {
+            const names = fullName.split(' ');
+            this.firstName = names[0] || '';
+            this.lastName = names[1] || '';
+        }
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    refreshToken: {
+        type: String,
+        unique: true
+    },
+    notifications: [{
+        type: notificationSchema.schema, 
+        ref: "Notification",
+        default: [],
+        required: false
+    }],
+    schedulePreferences: {
+        type: preferenceSchema,
+        default: null,
+        required: false
+    }
+});
+
 const preferenceSchema = new Schema({
     Monday: [{
         type: Number,
@@ -80,54 +128,6 @@ const preferenceSchema = new Schema({
             message   : '{VALUE} is not an integer value'
           }
     }]
-});
-
-const patientSchema = new Schema({
-    id: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    },
-    fullName: {
-        type: String,
-        // get fullName by concatenating first and last names
-        get: function () {
-            return `${this.firstName} ${this.lastName}`;
-        },
-        // set new fullName by splitting first and last names
-        set: function (fullName) {
-            const names = fullName.split(' ');
-            this.firstName = names[0] || '';
-            this.lastName = names[1] || '';
-        }
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    refreshToken: {
-        type: String,
-        unique: true
-    },
-    notifications: [{
-        type: notificationSchema.schema, 
-        ref: "Notification",
-        default: [],
-        required: false
-    }],
-    schedulePreferences: {
-        type: preferenceSchema,
-        default: null,
-        required: false
-    }
 });
 
 module.exports = mongoose.model("Patient", patientSchema);
