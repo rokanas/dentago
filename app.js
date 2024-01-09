c4ce61995ceae9d6d51a573b91a8e05a424042d7
Erik <elindstr@student.chalmers.se>
2024-01-09 01:26:49 +0100

    #7 Populate 'clinic' In Timeslot Response
    This commit populates the 'clinic' field in the Timeslot database queries so that the clinic info is made available to the front-end.

--- app.js
+++ app.js
@@ -184,7 +184,7 @@ client.on('message', async (topic, message) => {
                     dentist:    { $ne: null },
                     startTime:  { $gt: startDate },
                     endTime:    { $lt: endDate }
-                }).populate('dentist');
+                }).populate('clinic').populate('dentist');
 
                 // Assign status code and message
                 responseObject.status.code = 200;
@@ -249,7 +249,7 @@ client.on('message', async (topic, message) => {
                     dentist:    { $ne: null },
                     startTime:  { $gt: startDate },
                     endTime:    { $lt: endDate }
-                }).populate('dentist');
+                }).populate('clinic').populate('dentist');
 
                 // extract preferences object from patient resource
                 const preferences = patient.schedulePreferences.toObject();
